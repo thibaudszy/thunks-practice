c5079d923a8d9ce8d540c482279d676bb4651e47
thibaudszy <thivaudszy@gmail.com>
2020-11-04 11:53:09 +0100

    homePage works

---
+++ src/store/reducer.js
@@ -0,0 +1,7 @@
+// src/store/reducer.js
+import { combineReducers } from "redux";
+//import someFeatureReducer from "./someFeature/reducer";
+
+const reducer = combineReducers({});
+
+export default reducer;
